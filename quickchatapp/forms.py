0a12f87053ea71186c810e352687153ff20d5b39
ravi300601 <ravikb01.rb@gmail.com>
2024-04-05 22:43:44 +0530

    frontend for login and register

--- quickchatapp/forms.py
+++ quickchatapp/forms.py
@@ -5,11 +5,11 @@ from django.forms import ModelForm
 from .models import ChatMessage, Profile
 
 class UserForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter username'}))
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter firstname'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter lastname'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm username'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control', 'aria-label': 'Username', 'aria-describedby': 'username'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control', 'aria-label': 'Firstname', 'aria-describedby': 'firstname'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control', 'aria-label': 'Lastname', 'aria-describedby': 'lastname'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control', 'aria-label': 'Password', 'aria-describedby': 'password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirm', 'class': 'form-control', 'aria-label': 'Cpassword', 'aria-describedby': 'CPassword'}))
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "password1", "password2"]
